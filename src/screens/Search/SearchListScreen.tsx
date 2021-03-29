b9338fae67b4ff138363fe3b0dd9631a4cfd70de
shun0305 <shunbbra@icloud.com>
2021-03-29 17:37:01 +0900

    category flatlist

--- src/screens/Search/SearchListScreen.tsx
+++ src/screens/Search/SearchListScreen.tsx
@@ -1,23 +1,49 @@
 import React, {FC, useState} from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import Icons from 'react-native-vector-icons/MaterialIcons';
+
+import Colors from '../../constants/Color';
+import {categories} from '../../Data/CategoryData';
 
 const SearchListScreen: FC = props => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.header}>カテゴリーで検索</Text>
+      <FlatList
+        horizontal={true}
+        data={categories}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.categoryContainer}>
+            <Icons name={item.icon} color={Colors.primaryColor} size={35} />
+            <Text style={styles.categorytext}>{item.category}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'white',
+    paddingLeft: 15,
   },
   header: {
-    fontSize: 18,
+    fontSize: 21,
     fontWeight: '700',
+  },
+  categoryContainer: {
+    width: 70,
+    height: 70,
+    borderColor: Colors.primaryColor,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+  },
+  categorytext: {
+    color: Colors.primaryColor,
+    fontSize: 15,
   },
 });
 
