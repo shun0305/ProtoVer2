f36a1a2b0d3d4293488cd15d30902a235967f8d2
shun0305 <shunbbra@icloud.com>
2021-04-01 14:45:42 +0900

    firebase setting

--- src/screens/Message/MessageScreen.tsx
+++ src/screens/Message/MessageScreen.tsx
@@ -1,10 +1,9 @@
 import React, {FC} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {FIREBASE_API_KEY} from '@env';
 const MessageScreen: FC = () => {
   return (
     <View style={styles.container}>
-      <Text>{FIREBASE_API_KEY}</Text>
+      <Text>Message Screen</Text>
     </View>
   );
 };
