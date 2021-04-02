ee715a8a747ac812909ee7c386c682a49f201aff
shun0305 <shunbbra@icloud.com>
2021-04-03 08:11:46 +0900

    categoryで検索機能

--- src/screens/Search/SearchCategoryScreen.tsx
+++ src/screens/Search/SearchCategoryScreen.tsx
@@ -1,15 +1,19 @@
-import React, {FC} from 'react';
+import React, {FC, useState, useEffect} from 'react';
 import {
   View,
   Text,
   Image,
   TextInput,
   TouchableOpacity,
+  FlatList,
   StyleSheet,
 } from 'react-native';
+import Icons from 'react-native-vector-icons/MaterialIcons';
 
+import firebase from '../../constants/firebase';
 import Colors from '../../constants/Color';
 import Button from '../../components/UI/Button';
+import SearchBar from '../../components/UI/SearchBar';
 import WarnButton from '../../components/UI/Buttons/WarnButton';
 import ProfitButton from '../../components/UI/Buttons/ProfitButton';
 
@@ -19,10 +23,81 @@ interface DetailProp {
 
 const SearchCategoryScreen: FC<DetailProp> = props => {
   const {category} = props.route.params;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('posts')
+      .where('category', '==', category)
+      .onSnapshot(querySnapshot => {
+        const posts = [];
+        querySnapshot.docs.forEach(doc => {
+          const {
+            iconname,
+            text,
+            info,
+            address,
+            date,
+            profileImage,
+            username,
+          } = doc.data();
+          posts.push({
+            id: doc.id,
+            iconname,
+            text,
+            info,
+            address,
+            date,
+            profileImage,
+            username,
+          });
+        });
+        setPosts(posts);
+      });
+  }, []);
+
+  console.log(posts);
 
   return (
     <View style={styles.container}>
-      <Text>{category}</Text>
+      <View style={styles.searchbar}>
+        <SearchBar placeholder="地名で検索" />
+      </View>
+      <FlatList
+        data={posts}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() =>
+              props.navigation.navigate('detail', {
+                username: item.username,
+                profileImage: item.profileImage,
+                content: item.text,
+                place: item.address,
+                category: item.iconname,
+                info: item.info,
+                time: item.date,
+              })
+            }
+            style={styles.listItemContainer}>
+            <View style={styles.listUserContainer}>
+              {item.profileImage !== null ? (
+                <Image style={styles.image} source={{uri: item.profileImage}} />
+              ) : (
+                <View style={styles.imageNull}>
+                  <Icons name="person" color="gray" size={35} />
+                </View>
+              )}
+              <View>
+                <Text numberOfLines={1} style={styles.listName}>
+                  {item.text}
+                </Text>
+                <Text style={styles.listPlace}>{item.address}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
@@ -31,6 +106,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  searchbar: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
   },
   userContainer: {
     flexDirection: 'row',
@@ -65,6 +145,38 @@ const styles = StyleSheet.create({
   },
   content: {
     fontSize: 18,
+  },
+  listItemContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    marginHorizontal: 15,
+    justifyContent: 'space-between',
+  },
+  listUserContainer: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginRight: 20,
+  },
+  imageNull: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginRight: 20,
+    backgroundColor: Colors.lightGray,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listName: {
+    fontSize: 18,
+    width: '80%',
+  },
+  listPlace: {
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
 
--- src/screens/Search/SearchMapScreen.tsx
+++ src/screens/Search/SearchMapScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -9,16 +9,16 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import MapView from 'react-native-map-clustering';
-import {Marker} from 'react-native-maps';
+import {Marker, Callout} from 'react-native-maps';
 import Icons from 'react-native-vector-icons/MaterialIcons';
-import BottomSheet from 'react-native-bottomsheet-reanimated';
+import firebase from '../../constants/firebase';
 
 import Colors from '../../constants/Color';
 import ButtomModal from '../../components/UI/Modal/BottomModal';
 
 const INITIAL_REGION = {
-  latitude: 40.754932,
-  longitude: -73.984016,
+  latitude: 37.77493,
+  longitude: -122.4194166,
   latitudeDelta: 1.5,
   longitudeDelta: 1.5,
 };
@@ -29,18 +29,55 @@ const Screen = {
 };
 const snapPoints = [0, Screen.height / 2, '55%', '100%'];
 
-const SearchMapScreen = () => {
+const SearchMapScreen = props => {
   const mapRef = useRef();
   const openRef = useRef<number | null>(null);
   const animateToRegion = () => {
     let region = {
-      latitude: 52.6,
-      longitude: 18.3,
+      latitude: 37.77493,
+      longitude: -122.419416,
       latitudeDelta: 7.5,
       longitudeDelta: 7.5,
     };
     mapRef.current.animateToRegion(region, 2000);
   };
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('posts')
+      .onSnapshot(querySnapshot => {
+        const posts = [];
+        querySnapshot.docs.forEach(doc => {
+          const {
+            iconname,
+            text,
+            info,
+            address,
+            date,
+            profileImage,
+            username,
+            lat,
+            lng,
+          } = doc.data();
+          posts.push({
+            id: doc.id,
+            iconname,
+            text,
+            info,
+            address,
+            date,
+            profileImage,
+            username,
+            lat,
+            lng,
+          });
+        });
+        setPosts(posts);
+      });
+  }, []);
 
   return (
     <View>
@@ -51,34 +88,42 @@ const SearchMapScreen = () => {
         initialRegion={INITIAL_REGION}
         clusterColor={Colors.primaryColor}
         style={{width: 600, height: 900}}>
-        <Marker
-          coordinate={{latitude: 52.4, longitude: 18.7}}
-          pinColor="#D2691E">
-          <View style={styles.icon}>
-            <Icons name="directions-car" size={24} color="white" />
-          </View>
-        </Marker>
-        <Marker
-          coordinate={{latitude: 51.4, longitude: 18.7}}
-          pinColor="#D2691E">
-          <View style={styles.icon}>
-            <Icons name="attach-money" size={24} color="white" />
-          </View>
-        </Marker>
-        <Marker
-          coordinate={{latitude: 53.4, longitude: 16.7}}
-          pinColor="#D2691E">
-          <View style={styles.icon}>
-            <Icons name="smoking-rooms" size={24} color="white" />
-          </View>
-        </Marker>
-        <Marker
-          coordinate={{latitude: 52.4, longitude: 16.7}}
-          pinColor="#D2691E">
-          <View style={styles.icon}>
-            <Icons name="fastfood" size={24} color="white" />
-          </View>
-        </Marker>
+        {posts.map(item => {
+          return (
+            <Marker
+              key={item.id}
+              coordinate={{latitude: item.lat, longitude: item.lng}}
+              pinColor="#D2691E">
+              <Callout
+                style={styles.plainView}
+                // onPress={(e) => {
+                //   if (
+                //     e.nativeEvent.action === 'marker-inside-overlay-press' ||
+                //     e.nativeEvent.action === 'callout-inside-press'
+                //   ) {
+                //     return;
+                //   }
+                //  props.navigation.navigate('detail', {
+                //     username: item.username,
+                //     profileImage: item.profileImage,
+                //     content: item.text,
+                //     place: item.address,
+                //     category: item.iconname,
+                //     info: item.info,
+                //     time: item.date,
+                //   })
+                // }}
+              >
+                <View>
+                  <Text>this is plain view</Text>
+                </View>
+              </Callout>
+              <View style={styles.icon}>
+                <Icons name={item.iconname} size={24} color="white" />
+              </View>
+            </Marker>
+          );
+        })}
       </MapView>
       <ButtomModal snapPoints={snapPoints} openRef={openRef} />
     </View>
@@ -130,6 +175,9 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  plainView: {
+    width: 100,
   },
 });
 
