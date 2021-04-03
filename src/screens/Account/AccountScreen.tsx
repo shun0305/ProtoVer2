0b281c2da4f8a0c9e9a7159afc6019f61be42516
shun0305 <shunbbra@icloud.com>
2021-04-03 13:07:38 +0900

    profile image機能

--- src/screens/Account/AccountScreen.tsx
+++ src/screens/Account/AccountScreen.tsx
@@ -1,9 +1,20 @@
-import React, {FC} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {FC, useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
+import Icons from 'react-native-vector-icons/MaterialIcons';
 
 import firebase from '../../constants/firebase';
 
 const AccountScreen: FC = props => {
+  const [img, setImg] = useState(null);
+
   var user = firebase.auth().currentUser;
   var name, email, photoUrl, uid, emailVerified;
 
@@ -28,6 +39,48 @@ const AccountScreen: FC = props => {
       <Text>Account Screen</Text>
       <Button title="sign out" onPress={signout} />
       <Text>{name}</Text>
+      {img !== null ? (
+        <TouchableOpacity
+          style={styles.imageButton}
+          onPress={() =>
+            ImagePicker.openPicker({
+              width: 1000,
+              height: 1000,
+              cropping: true,
+              cropperCircleOverlay: true,
+            }).then(image => {
+              // 画像トリミング後に行いたい処理を記述
+              console.log(image);
+              setImg(image);
+            })
+          }>
+          <Image style={styles.image} source={{uri: img.path}} />
+          <Text>画像変更</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={styles.imageButton}
+          onPress={() =>
+            ImagePicker.openPicker({
+              width: 1000,
+              height: 1000,
+              cropping: true,
+              cropperCircleOverlay: true,
+            }).then(image => {
+              // 画像トリミング後に行いたい処理を記述
+              console.log(image);
+              setImg(image);
+            })
+          }>
+          <Icons
+            name="add-photo-alternate"
+            color="gray"
+            size={35}
+            style={styles.inputIcon}
+          />
+          <Text>画像変更</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -37,6 +90,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  imageNull: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: 'red',
   },
 });
 
